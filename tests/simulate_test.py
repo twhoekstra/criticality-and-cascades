3f139210438c10389771f5e1f46612057954b9da
twhoekstra <t.w.hoekstra@student.tudelft.nl>
2023-10-29 18:04:45 +0100

    Added plotting functionality

--- tests/simulate_test.py
+++ tests/simulate_test.py
@@ -3,7 +3,6 @@ from utils.simulate import RecurrentNeuralNetwork
 
 import matplotlib.pyplot as plt
 
-
 class RecurrentNeuralNetworkTestCase(unittest.TestCase):
 
     def test_create(self):
@@ -12,6 +11,21 @@ class RecurrentNeuralNetworkTestCase(unittest.TestCase):
     def test_sim_run(self):
         rnn = RecurrentNeuralNetwork(n=10)
         rnn.sim(1200)
+
+    def test_sim_plot_state(self):
+        rnn = RecurrentNeuralNetwork(n=10)
+        rnn.sim(1200)
+        rnn.plot_state()
+
+    def test_sim_plot_connectivity(self):
+        rnn = RecurrentNeuralNetwork(n=100)
+        rnn.sim(10)
+        rnn.plot_connectivity()
+
+    def test_sim_plot_state_big(self):
+        rnn = RecurrentNeuralNetwork(n=100, g=10)
+        rnn.sim(1200)
+        rnn.plot_state()
 
 
 if __name__ == '__main__':
--- utils/simulate.py
+++ utils/simulate.py
@@ -1,6 +1,48 @@
-from brian2 import *
+from dataclasses import dataclass
+from typing import Optional
+
+from brian2 import (defaultclock, ms, Hz, mV, NeuronGroup, Synapses,
+                    StateMonitor, SpikeMonitor, PopulationRateMonitor,
+                    PoissonInput, Network, collect)
 import matplotlib.pyplot as plt
 import numpy as np
+
+
+class SimulationNotRunException(Exception):
+    pass
+
+
+class StateResults:
+    """Results of StateMonitor"""
+
+    def __init__(self, monitor: StateMonitor):
+        self.v_mV: np.ndarray = monitor.v / mV
+        self.t_ms: np.ndarray = monitor.t / ms
+
+    def as_array(self):
+        return np.vstack((self.t_ms, self.v_mV))
+
+
+class SpikeResults:
+    """Results of StateMonitor"""
+
+    def __init__(self, monitor: SpikeMonitor):
+        self.i: np.ndarray = monitor.i
+        self.t_ms: np.ndarray = monitor.t / ms
+
+    def as_array(self):
+        return np.vstack((self.t_ms, self.i))
+
+
+class RateResults:
+    """Results of RateMonitor"""
+
+    def __init__(self, monitor: PopulationRateMonitor):
+        self.rate: np.ndarray = monitor.rate / Hz
+        self.t_ms: np.ndarray = monitor.t / ms
+
+    def as_array(self):
+        return np.vstack((self.t_ms, self.rate))
 
 
 class RecurrentNeuralNetwork:
@@ -50,19 +92,28 @@ class RecurrentNeuralNetwork:
     }
 
     def __init__(self, n: int = 128, g: float = 3, p_c: float = 0.04, gamma=0.8,
-                 p_ext: float = 6E-4, nu_ext_over_nu_thr = 0.9):
+                 p_ext: float = 6E-4, nu_ext_over_nu_thr=0.9):
 
+        self.state_monitor = None
+        self.spike_monitor = None
+        self.rate_monitor = None
+        self.net: Optional[Network] = None
+        self.neurons = None
+        self.inhib_synapses = None
+        self.exc_synapses = None
         self.n = n
+        self.n_e = None
         self.p_c = p_c
         self.g = 3
         self.gamma = gamma
         self.p_ext = p_ext
+        self.leader_neuron_idx = None
 
         self.nu_ext_over_nu_thr = nu_ext_over_nu_thr
 
-        self.v = None
-        self.t = None
-        self.state_data = None
+        self.state_results: Optional[StateResults] = None
+        self.spike_results: Optional[SpikeResults] = None
+        self.rate_results: Optional[RateResults] = None
 
     def sim(self, sim_time_ms):
 
@@ -77,12 +128,9 @@ class RecurrentNeuralNetwork:
         J = self.synapse_params['J_mV'] * mV
         D = self.synapse_params['D_ms'] * ms
 
-        sim_time = sim_time_ms * ms
-
         # network parameters
         N_E = round(self.gamma * self.n)
         N_I = self.n - N_E
-
         epsilon = 0.1
         C_E = epsilon * N_E
         C_ext = C_E
@@ -108,28 +156,28 @@ class RecurrentNeuralNetwork:
         excitatory_neurons = neurons[:N_E]
         inhibitory_neurons = neurons[N_E:]
 
-        exc_synapses = Synapses(excitatory_neurons,
-                                target=neurons,
-                                on_pre="v += J",
-                                delay=D)
-        exc_synapses.connect(p=self.gamma, condition='i!=j')
+        self.exc_synapses = Synapses(excitatory_neurons,
+                                     target=neurons,
+                                     on_pre="v += J",
+                                     delay=D)
+        self.exc_synapses.connect(p=self.p_c, condition='i!=j')
 
-        inhib_synapses = Synapses(inhibitory_neurons,
-                                  target=neurons,
-                                  on_pre="v += -g*J",
-                                  delay=D)
-        inhib_synapses.connect(p=self.gamma,
-                               condition='i!=j')  # No self connections
+        self.inhib_synapses = Synapses(inhibitory_neurons,
+                                       target=neurons,
+                                       on_pre="v += -g*J",
+                                       delay=D)
+        self.inhib_synapses.connect(p=self.p_c,
+                                    condition='i!=j')  # No self connections
 
         nu_ext = self.nu_ext_over_nu_thr * nu_thr
-
         external_poisson_input = PoissonInput(
             target=neurons, target_var="v", N=self.n, rate=rate,
             weight=J
         )
 
-        leading_idx = np.random.randint(0, N_E)
-        leading_neuron = excitatory_neurons[leading_idx:leading_idx+1]
+        idx = np.random.randint(0, N_E)
+        self.leader_neuron_idx = idx
+        leading_neuron = excitatory_neurons[idx:idx + 1]
 
         leading_neuron_input = PoissonInput(
             target=leading_neuron,
@@ -137,36 +185,124 @@ class RecurrentNeuralNetwork:
             weight=10 * mV
         )
 
-        rate_monitor = PopulationRateMonitor(neurons)
+        monitors = self.setup_monitors(neurons)
 
+        self.net = Network(collect())
+        self.net.add(monitors)
+        self.net.add((self.exc_synapses, self.inhib_synapses))
+
+        self.neurons = neurons
+        self.n_e = N_E
+
+        self.net.run(sim_time_ms * ms, report='text')
+
+        self.rate_results = RateResults(self.rate_monitor)
+        self.state_results = StateResults(self.state_monitor)
+        self.spike_results = SpikeResults(self.spike_monitor)
+
+        return 1
+
+    def setup_monitors(self, neurons, i=50):
+        self.rate_monitor = PopulationRateMonitor(neurons)
         # record from the first 50 excitatory neurons
-        spike_monitor = SpikeMonitor(excitatory_neurons[:50])
+        self.spike_monitor = SpikeMonitor(neurons[:i])
+        self.state_monitor = StateMonitor(neurons[:i], 'v', record=True)
 
-        state_monitor = StateMonitor(neurons, 'v', record=True)
+        return self.rate_monitor, self.spike_monitor, self.state_monitor
 
-        net = Network(collect())
+    def plot_connectivity(self):
+        if self.exc_synapses is None or self.inhib_synapses is None:
+            raise SimulationNotRunException('Error, no results yet. '
+                                            'Did you run the simulation?')
 
-        net.run(sim_time, report='text')
+        fig, axs = plt.subplots(1, 2)
 
-        self.v = state_monitor.v / mV
-        self.t = state_monitor.t / ms
+        axs[0].plot(np.zeros(self.n), np.arange(self.n), 'ok', ms=10)
+        axs[0].plot(np.ones(self.n), np.arange(self.n), 'ok', ms=10)
 
+        n_prev = 0
+        for S, c, offset in zip(
+                (self.exc_synapses, self.inhib_synapses),
+                ('b', 'r'),
+                (0, self.n_e)):
+            Ns = len(S.source)
+            Nt = len(S.target)
 
-    def plot(self,
-        fig,
-        title=None,
-        t_range: tuple = None,
-        rate_range: tuple = None,
-        rate_tick_step: float = 50,):
+            for i, j in zip(S.i, S.j):
+                axs[0].plot([0, 1], [i + offset, j], '-', c=c)
 
+            axs[0].set_xticks([0, 1], ['Source', 'Target'])
+            axs[0].set_ylabel('Neuron index')
+            axs[0].set_xlim(-0.1, 1.1)
+            axs[0].set_ylim(-1, max(Ns, Nt))
+
+            axs[1].plot(np.asarray(S.i) + offset, np.asarray(S.j), 'o', c=c,
+                        alpha=0.5)
+            axs[1].set_xlabel('Source index')
+            axs[1].set_ylabel('Target index')
+
+        fig.subplots_adjust(wspace=0.4)
+        fig.show()
+
+    def plot_state(self, show=True):
+
+        if self.spike_results is None:
+            raise SimulationNotRunException('Error, no results yet. '
+                                            'Did you run the simulation?')
+
+        fig, axs = plt.subplots(2, 1, sharex=True)
+
+        axs[0].imshow(self.state_results.v_mV,
+                      aspect='auto',
+                      interpolation='nearest',
+                      cmap='Reds',
+                      extent=(
+                          self.state_results.t_ms[0],
+                          self.state_results.t_ms[-1],
+                          0,
+                          self.state_results.v_mV.shape[0]
+                      ))
+
+        axs[0].set_xlabel('Time [ms]')
+        axs[0].set_ylabel('Neuron')
+
+        traces = self.state_results.v_mV.T
+
+        axs[1].plot(self.state_results.t_ms, traces, c='blue')
+        if self.leader_neuron_idx < traces.shape[1]:
+            axs[1].plot(self.state_results.t_ms, traces[:, self.leader_neuron_idx],
+                        label='Leader neuron', c='red')
+
+        axs[1].set_ylabel('Voltage [mV]')
+        axs[1].legend()
+
+        fig.subplots_adjust(hspace=0)
+
+        if show:
+            fig.show()
+
+        return 1
+
+    def plot_spikes(self,
+                    title=None,
+                    t_range: tuple = None,
+                    rate_range: tuple = None,
+                    show=True,
+                    rate_tick_step: float = 50, ):
+
+        if self.spike_results is None or self.rate_results is None:
+            raise SimulationNotRunException('Error, no results yet. '
+                                            'Did you run the simulation?')
+
+        fig = plt.figure()
         fig.suptitle(title)
 
         gs = fig.add_gridspec(ncols=1, nrows=2, height_ratios=[4, 1])
 
         ax_spikes, ax_rates = gs.subplots(sharex="col")
 
-        ax_spikes.plot(self.spike_monitor.t / ms, self.spike_monitor.i, "|")
-        ax_rates.plot(self.rate_monitor.t / ms, self.rate_monitor.rate / Hz)
+        ax_spikes.plot(self.spike_results.as_array(), "|")
+        ax_rates.plot(self.rate_results.as_array())
 
         ax_spikes.set_yticks([])
 
@@ -187,4 +323,7 @@ class RecurrentNeuralNetwork:
 
         fig.subplots_adjust(hspace=0)
 
-        return fig
+        if show:
+            fig.show()
+
+        return 1
